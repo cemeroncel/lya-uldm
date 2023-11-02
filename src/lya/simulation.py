c01b0a430067a754461857b53d0e6cd0b8bae238
Cem Eroncel <cemeroncel@gmail.com>
2023-11-02 10:36:49 +0300

    Added all the MPGadget config options to the Simulation class.

--- src/lya/simulation.py
+++ src/lya/simulation.py
@@ -7,7 +7,8 @@ class Simulation:
                  OmegaBaryon: float, OmegaLambda: float, h: float,
                  output_list: list[float], cpu_time_hr: float,
                  z_start: float = 99., seed: int = 960169,
-                 As: float = 2.215e-9, ns: float = 0.971):
+                 As: float = 2.215e-9, ns: float = 0.971,
+                 a_end: float = 1.):
         # Input parameters
         self.box = box
         self.ngrid = ngrid
@@ -20,6 +21,7 @@ class Simulation:
         self.seed = seed
         self.As = As
         self.ns = ns
+        self.a_end = a_end
 
         # Hard-coded parameters
         self.output = 'output'
@@ -61,15 +63,76 @@ class Simulation:
         config = ConfigObj()
         config.filename = 'paramfile.gadget'
 
+        # Required parameters
         config['InitCondFile'] = self.output + '/' + self.IC
         config['OutputDir'] = self.output
         config['OutputList'] = self.output_list  # Check if this parses correctly.
         config['TimeLimitCPU'] = 60*60*self.cpu_time_hr
+        config['SnapshotWithFOF'] = self.snapshot_with_fof
+        config['BlackHoleOn'] = self.black_hole_on
+        config['StarformationOn'] = self.star_formation_on
+        config['WindOn'] = self.wind_on
+
+        # Cosmology parameters
+        config['TimeMax'] = self.a_end
+        config['Omega0'] = self.OmegaMatter
+        config['OmegaBaryon'] = self.OmegaLambda
+        config['HubbleParam'] = self.h
+        config['RadiationOn'] = 1
+
+        # Cooling model parameters
+        config['CoolingOn'] = self.cooling_on
+        if self.cooling_on == 1:
+            # TODO: Check if this file exists
+            config['TreeCoolFile'] = 'TREECOOL'
+        config['HeliumHeatOn'] = self.helium_heat_on
+        if self.helium_heat_on == 1:
+            config['HeliumHeatThresh'] = self.helium_heat_thresh
+            config['HeliumHeatAmp'] = self.helium_heat_amp
+            config['HeliumHeatExp'] = self.helium_heat_exp
+
+        # Star formation model parameters
+        if self.star_formation_on == 1:
+            config['StarformationCriterion'] = self.sf_criterion
+            config['QuickLymanAlphaProbability'] = self.quick_lya
+            config['CritOverDensity'] = self.crit_over_density
+
+        # Wind (Stellar Feedback Model Parameters)
+        config['WindModel'] = 'nowind'
+
+        # Other parameters
+        config['DensityIndependentSphOne'] = self.density_independent_sph
+        config['HydroOne'] = self.hydro
+        config['DensityKernelType'] = self.density_kernel
+        config['MaxMemSizePerNode'] = self.max_mem_size_per_node
+        config['InitGasTemp'] = self.init_gas_temp
+        config['MinGasTemp'] = self.min_gas_temp
 
 
 class LyaSimulation(Simulation):
-    def __init__(self, **kwargs):
+    def __init__(self,
+                 helium_heat_thresh: float = 10.,
+                 helium_heat_amp: float = 1.,
+                 helium_heat_exp: float = 1.,
+                 **kwargs):
         super().__init__(**kwargs)
         self.produce_gas = 1
         self.different_transfer = 1
         self.scale_dep_velocity = 1
+        self.snapshot_with_fof = 0
+        self.black_hole_on = 0
+        self.star_formation_on = 1
+        self.wind_on = 0
+        self.cooling_on = 1
+        self.helium_heat_on = 1
+        self.helium_heat_thresh = helium_heat_thresh
+        self.helium_heat_amp = helium_heat_amp
+        self.helium_heat_exp = helium_heat_exp
+        self.sf_criterion = 'density'
+        self.quick_lya = 1
+        self.crit_over_density = 1000
+        self.density_independent_sph = 0
+        self.hydro = 1
+        self.density_kernel = 'cubic'
+        self.init_gas_temp = 270.
+        self.min_gas_temp = 100.
